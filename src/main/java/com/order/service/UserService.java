2e60f2bf3f461f163f446ee78f9f4b9e13f8f4c0
unknown <2476056494@qq.com>
2019-10-24 09:37:28 +0800

    补充hystrix

--- src/main/java/com/order/service/UserService.java
+++ src/main/java/com/order/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class UserService {
 	
@@ -24,10 +26,16 @@ public class UserService {
 	 * @param userId
 	 * @return
 	 */
+	@HystrixCommand(fallbackMethod = "validUserFallback")
 	String validUser(String userId) {
 		String url = String.format("http://%s/user/validUser/%s", userService,userId);
 		String result = restTemplate.postForObject(url, null, String.class);
 		logger.info(result);
 		return result;
+	}
+	// 降级方法需要和目标方法参数和返回类型一致
+	public String validUserFallback(String userId){
+		logger.info("validUser fallback");
+		return "validUser fallback";
 	}
 }
